625f28804b77f7b5e8808792bc28ead6a2817438
Celso Fabri Junior <celso.fabri@ifood.com.br>
2022-09-04 21:34:38 -0300

    feat: add and remove unnecessary files

--- src/containers/App/index.js
+++ src/containers/App/index.js
@@ -1,36 +1,13 @@
-import React, { useEffect, useState } from 'react';
-import { GIRIAS } from './constants';
+import React from 'react';
+import Sidebar from 'components/Sidebar';
+import Slang from 'components/Slang';
 
 const App = () => {
-  const [giria, setGiria] = useState({
-    name: '', 
-    description: '', 
-    examples: []
-  })
-
-  const getRandomItem = (arr) => {
-    const randomIndex = Math.floor(Math.random() * arr.length);
-    const item = arr[randomIndex];
-  
-    setGiria(item);
-  }
-
-  useEffect(() => {
-    getRandomItem(GIRIAS)
-  }, [])
 
   return (
     <div>
-      <h2>{giria?.name}</h2>
-      <p>{giria?.description}</p>
-      <ul>
-        {giria?.examples.map((example, index) => {
-          return (
-            <li key={index}>{example}</li>
-          )
-        })}
-      </ul>
-      <button onClick={() => getRandomItem(GIRIAS)} type="button">Update</button>
+      <Sidebar/>
+      <Slang/>
     </div>
   );
 }
